f311e2b79491a755f7c77007f9af94b912b60d6e
Anthony Super <subtlehyperbole.thesuper@gmail.com>
2014-10-28 15:36:31 -0600

    Initial Commit

---
+++ class-1/ClosureNumberJudge/ClosureNumberJudge/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  ClosureNumberJudge
+//
+//  Created by Anthony Something on 10/28/14.
+//  Copyright (c) 2014 Anthony Super. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
